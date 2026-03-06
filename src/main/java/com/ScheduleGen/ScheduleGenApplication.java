33f094b5b53861eafa5927e2a09887263d3eed4b
victor <teamogurcic6002@gmail.com>
2026-03-06 23:20:55 +0200

    initial commit

---
+++ src/main/java/com/ScheduleGen/ScheduleGenApplication.java
@@ -0,0 +1,13 @@
+package com.ScheduleGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ScheduleGen/ScheduleGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScheduleGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
